4244f57fa22df7173afdf4d128c740dde53a47cd
MikelazDotPy <onextopplay@gmail.com>
2025-07-04 11:13:11 +0300

    TRI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,7 +11,7 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2397055294",
+        serviceWorkerVersion: "820280545",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const appRunner = await engineInitializer.initializeEngine({useColorEmoji: useColorEmoji});
