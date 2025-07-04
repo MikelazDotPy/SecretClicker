84e37b55a33a5592c81b386bb9ab13b7c1aa8ce4
MikelazDotPy <onextopplay@gmail.com>
2025-07-04 11:27:45 +0300

    1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,15 +39,15 @@ const RESOURCES = {"flutter.js": "76f08d47ff9f5715220992f993002504",
 "assets/NOTICES": "76e2ce694846085a87812d4bef225498",
 "assets/AssetManifest.bin.json": "992ba04d5892193176350dad1f95a2ec",
 "assets/AssetManifest.json": "5515896244b15ec7f3fc5ce45479796d",
-"assets/app/app.zip.hash": "3c8d7f07ce701c0db64f6f51302fa9f7",
-"assets/app/app.zip": "acf5a1bff867ef67cfe709040c593e92",
+"assets/app/app.zip.hash": "85674507633a46067e71c076bdb47ade",
+"assets/app/app.zip": "730f3882ab384a83244e23a5c380a620",
 "assets/AssetManifest.bin": "5e4c69c57b629fa3c0fbf78c71c4db94",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "icons/Icon-maskable-512.png": "1b5e2bffe69da59cfd351c35c744bcdc",
 "icons/Icon-maskable-192.png": "817caa1985cf73eaaa8dd02dda218970",
 "icons/Icon-512.png": "1b5e2bffe69da59cfd351c35c744bcdc",
 "icons/Icon-192.png": "817caa1985cf73eaaa8dd02dda218970",
-"flutter_bootstrap.js": "9cddc3027c4d7068f03e48426e396f29"};
+"flutter_bootstrap.js": "77c009069d0dacbeba7d875ba746ba3e"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
